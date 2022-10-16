8470a4984fa312978b57b7e84e2055a8c0b751b8
Kaiyum <kaiyumce06rumman@gmail.com>
2022-10-16 08:50:03 +0600

    init with two item

---
+++ Runtime/ReadOnlyAttribute.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AttributeExt
+{
+    public class CanNotEditAttribute : PropertyAttribute
+    {
+
+    }
+}
