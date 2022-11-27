b7f0756c2f5a4a26d5612f49dbfd39d0630b5ac5
kaiyumPC <kaiyumcg@gmail.com>
2022-11-27 21:05:24 +0600

    serref issue fixed

--- Editor/SerRefEditor/DefaultAttributes/SerializeReferenceMenuAttribute/SerializeReferenceMenuAttributeDrawer.cs
+++ Editor/SerRefEditor/DefaultAttributes/SerializeReferenceMenuAttribute/SerializeReferenceMenuAttributeDrawer.cs
@@ -26,3 +26,4 @@ namespace AttributeExt
     }
 }
 #endif
+
---
+++ Runtime/SerRef/DefaultAttributes/SerializeReferenceButtonAttribute/SerializeReferenceButtonAttribute.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using UnityEngine;
+
+namespace AttributeExt
+{
+    [AttributeUsage(AttributeTargets.Field)]
+    public class SerializeReferenceButtonAttribute : PropertyAttribute
+    {
+
+    }
+}
